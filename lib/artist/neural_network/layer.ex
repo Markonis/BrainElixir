ee8b362b206ec0e04f9aff364a2b91bffdc9129f
Marko Pavlovic <contact.pavlovic@gmail.com>
2016-10-01 11:29:47 +0200

    Implement Layer.set_outputs function

--- lib/artist/neural_network/layer.ex
+++ lib/artist/neural_network/layer.ex
@@ -13,4 +13,11 @@ defmodule Artist.NeuralNetwork.Layer do
       GenServer.cast(src_pid, {:connect_to, dest_pid})
     end
   end
+
+  def set_outputs(layer, outputs) do
+    List.zip([layer.neurons, outputs])
+    |> Enum.each(fn {neuron_pid, output} ->
+      GenServer.cast(neuron_pid, {:set_output, output})
+    end)
+  end
 end
--- test/artist/layer_test.exs
+++ test/artist/layer_test.exs
@@ -23,4 +23,17 @@ defmodule Artist.NeuralNetwork.LayerTest do
 
     assert length(first_state.out_conn) == 2
   end
+
+  test "set_outputs" do
+    layer = Layer.create(3)
+    Layer.set_outputs(layer, [1, 2, 3])
+
+    :timer.sleep(10)
+
+    outputs = Enum.map layer.neurons, fn neuron_pid ->
+      GenServer.call(neuron_pid, :get_state).output
+    end
+
+    assert outputs == [1, 2, 3]
+  end
 end
