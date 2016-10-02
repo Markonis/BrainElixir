442bf14599abd6904accb8c610f479397071b06d
Marko Pavlovic <contact.pavlovic@gmail.com>
2016-10-02 17:10:51 +0200

    Add sample xor network trainer to Artist module

--- lib/artist.ex
+++ lib/artist.ex
@@ -1,2 +1,20 @@
 defmodule Artist do
+
+  alias Artist.NeuralNetwork
+
+  def xor do
+    network = NeuralNetwork.create([2, 4, 2, 1])
+
+    input_output_pairs = [
+      {[0, 0], [0]}, {[0, 1], [1]},
+      {[1, 0], [1]}, {[1, 1], [0]}]
+
+    Enum.each 1..15_000, fn _ ->
+      Enum.each input_output_pairs, fn {inputs, target_outputs} ->
+        NeuralNetwork.train(network, inputs, target_outputs)
+      end
+    end
+
+    network
+  end
 end
