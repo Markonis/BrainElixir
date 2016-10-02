ea95e148099bb8792a4de477938bf8ea09d594e2
Marko Pavlovic <contact.pavlovic@gmail.com>
2016-10-02 15:54:19 +0200

    Implement Layer.adjust_weights

--- lib/artist/neural_network/layer.ex
+++ lib/artist/neural_network/layer.ex
@@ -53,4 +53,11 @@ defmodule Artist.NeuralNetwork.Layer do
       GenServer.call(neuron_pid, {:prop_backward, target_output})
     end)
   end
+
+  def adjust_weights(layer, target_outputs) do
+    List.zip([layer.neurons, target_outputs])
+    |> Enum.each(fn {neuron_pid, target_output} ->
+      GenServer.call(neuron_pid, {:adjust_weights, target_output})
+    end)
+  end
 end
