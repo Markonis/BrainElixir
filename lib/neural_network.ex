150f51efe8cafab69bf8e9fda7edaeb581be540e
Marko Pavlovic <mare.nis@live.com>
2016-10-07 13:39:28 +0200

    Serialize and deserialize network into JSON

--- lib/neural_network.ex
+++ lib/neural_network.ex
@@ -81,15 +81,34 @@ defmodule NeuralNetwork do
   end
 
   def serialize(network) do
-    %{
+    Poison.encode! %{
       configuration: get_configuration(network),
       in_conns: get_in_conns(network)
     }
   end
 
-  def deserialize(data) do
-    NeuralNetwork.create(data.configuration)
-    |> NeuralNetwork.set_in_conns(data.in_conns)
+  def deserialize(json) do
+    data = Poison.decode! json
+
+    network = data
+    |> Map.get("configuration")
+    |> NeuralNetwork.create
+
+    in_conns = data
+    |> Map.get("in_conns")
+    |> to_in_conn_structs
+
+    NeuralNetwork.set_in_conns(network, in_conns)
+  end
+
+  def to_in_conn_structs(network_in_conns) do
+    Enum.map network_in_conns, fn layer_in_conns ->
+      Enum.map layer_in_conns, fn neuron_in_conns ->
+        Enum.map neuron_in_conns, fn in_conn ->
+          NeuralNetwork.Connection.from_map(in_conn)
+        end
+      end
+    end
   end
 
   def process(network, inputs) do
--- lib/neural_network/connection.ex
+++ lib/neural_network/connection.ex
@@ -1,3 +1,13 @@
 defmodule NeuralNetwork.Connection do
   defstruct weight: 0, value: 0, index: 0
+
+  alias NeuralNetwork.Connection
+
+  def from_map(map) do
+    %Connection{
+      weight: Map.get(map, "weight"),
+      value: Map.get(map, "value"),
+      index: Map.get(map, "index")
+    }
+  end
 end
