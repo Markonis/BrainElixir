2bc42fa84ec5a55f3ee67e8b8ddfc5645c9a0eba
Marko Pavlovic <mare.nis@live.com>
2016-10-09 11:27:09 +0200

    Add escript configuration to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule BrainElixir.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: CommandLine],
      deps: deps]
   end
 
