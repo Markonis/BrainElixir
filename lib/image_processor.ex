c3cae55583beee1ead14808be285ebbbc38f6989
Marko Pavlovic <mare.nis@live.com>
2016-10-11 11:03:19 +0200

    Fix ImageProcessor.normalize_values and allow single-channel pixels

--- lib/image_processor.ex
+++ lib/image_processor.ex
@@ -13,6 +13,7 @@ defmodule ImageProcessor do
 
   def to_grayscale(pixels) do
     Enum.map pixels, fn
+      {v}           -> v
       {r, g, b, _a} -> (r + g + b) / 3
       {r, g, b}     -> (r + g + b) / 3
     end
@@ -20,12 +21,16 @@ defmodule ImageProcessor do
 
   def flatten_pixels(pixels) do
     Enum.flat_map pixels, fn
-      {r, g, b} -> [r, g, b]
-      {r, g, b, a} -> [r, g, b, a]
+      {v}           -> [v]
+      {r, g, b}     -> [r, g, b]
+      {r, g, b, a}  -> [r, g, b, a]
     end
   end
 
   def normalize_values(values) do
-    Enum.map values, fn value -> value / 255 end
+    max = Enum.max values
+    Enum.map values, fn value ->
+      2 * (value / max) - 1
+    end
   end
 end
